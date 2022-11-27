3812f54f656faf986d1b2f02cf8e0d27c3c995b7
koco67 <koco67.kd@gmail.com>
2022-11-27 18:29:04 +0100

    BoardTests and GameplayTests- nicht implementiert

---
+++ src/tests/BoardTests.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+public class BoardTests {
+
+    @Test
+    public void allShipsPlacedTest() {
+    }
+
+    @Test
+    public void shootingAtCoordinateTest() {
+    }
+
+    @Test
+    public void shootAndMissTest() {
+    }
+
+    @Test
+    public void shootAndHitTest() {
+    }
+
+    @Test
+    public void updateBoardTest() {
+    }
+}
---
+++ src/tests/GameplayTests.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+public class GameplayTests {
+
+    @Test
+    public void gameStartsTest() {
+    }
+
+    @Test
+    public void changeTurnsWhenMissedShot() {
+    }
+
+    @Test
+    public void extraTurnWhenHitShot() {
+    }
+
+    @Test
+    public void addPointWhenShipDown() {
+    }
+
+    @Test
+    public void getScoreTest() {
+    }
+
+    @Test
+    public void endGameTest() {
+    }
+}
