2a907b1d4522bf8a6f73b7a2b3505c0028d516e8
koco67 <koco67.kd@gmail.com>
2022-11-27 18:36:54 +0100

    added NetworkTests - nicht implementiert

---
+++ src/tests/NetworkTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class NetworkTests {
+    // TODO: NetworkTests implementieren
+
+    @Test
+    public void hostConnectedTest() {
+    }
+
+    @Test
+    public void Player2ConnectedTest() {
+    }
+}
